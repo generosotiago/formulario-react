a9d4653f98aa2cd757b1de0898acc4cebccc317f
Tiago Generoso <tiagogeneroso47@gmail.com>
2024-06-13 01:01:19 -0300

    mongo

---
+++ Procfile
@@ -0,0 +1,2 @@
+# Procfile
+web: node server/server.js
--- server/server.js
+++ server/server.js
@@ -1,34 +1,40 @@
+// server/server.js
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3001;
-const cors = require('cors'); // Importe o middleware CORS
 
+// Configuração do CORS
 app.use(cors());
 
-// Conexão com o MongoDB
-const dbURI = "mongodb+srv://tiagogeneroso47:VTwZjvGgcTrQ7dl7@formulario.98dkg0j.mongodb.net/formulario?retryWrites=true&w=majority";
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Conectar ao MongoDB
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/myLocalDb";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado ao MongoDB'))
   .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
-// Definir o esquema e o modelo do Mongoose
+// Definição do Schema e Model
 const formDataSchema = new mongoose.Schema({
   nome: { type: String, required: false },
   email: { type: String, required: false },
   dataNascimento: { type: Date, required: false },
   cpf: { type: String, required: false }
 });
-
 const FormData = mongoose.model('FormData', formDataSchema);
 
-app.use(express.json());
-
+// Rota de teste
 app.get('/', (req, res) => {
   res.send('Bem-vindo à nossa aplicação!');
 });
 
-// Endpoint para receber os dados do formulário e salvar no MongoDB
+// Rota para salvar dados do formulário
 app.post('/api/formulario', async (req, res) => {
   const formData = new FormData(req.body);
   try {
@@ -40,6 +46,16 @@ app.post('/api/formulario', async (req, res) => {
   }
 });
 
+// Servir arquivos estáticos da aplicação React em produção
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
